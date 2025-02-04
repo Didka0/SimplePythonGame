8815306b6616cccd4802730b9686581d5c4447eb
Diana Dovhun <dianadovgun4@gmail.com>
2025-02-04 16:48:43 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+answer = input("Hello, let's start?: ")
+
+answer = answer.lower()
+
+if answer == "yes":
+    print("Players info: \n Player 1 health - 100 \n Player 1 mana - 100 \n \n Player 2 health - 100 \n Player 2 mana - 100")
+else:
+    print("See you later!")
+    exit()
+
