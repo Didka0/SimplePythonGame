30bc9cfa3e8219bc17ad911cfec6a785181aef3d
Diana Dovhun <dianadovgun4@gmail.com>
2025-02-05 00:06:52 +0200

    Add player1 turn

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def print_game_rules():
     print("Rules: \n Each player can choose one of those commands to do on his turn:")
     print("Attack ( Deals a random number damage from 10 to 20 to the other player )")
@@ -5,18 +7,28 @@ def print_game_rules():
     print("Cast fireball ( Costs 40 mana and deals 30 damage to the other player )")
     print("Restore mana ( Restores 20 mana )")
 
+def player1_turn():
+    name1 = input("\n Enter your name,Player 1: ")
+    print("Choose your move,", name1,":", end='')
+    player_choice1 = input(" Attack,Heal,Cast fireball or Restore mana? ")
+
+    if player_choice1 == "Attack":
+        print("You dealt: ",random.choice(range(10, 20)), " damage")
+
 def main():
     print_game_rules()
 
-    answer = input("Let's start?: ")
+    answer = input("\n Let's start?: ")
 
     answer = answer.lower()
 
     if answer == "yes":
-        print("Players info: \n Player 1 health - 100 \n Player 1 mana - 100 \n \n Player 2 health - 100 \n Player 2 mana - 100")
+        print("\nPlayers info: \n Player 1 health - 100 \n Player 1 mana - 100 \n \n Player 2 health - 100 \n Player 2 mana - 100")
     else:
         print("See you later!")
         exit()
-    
+
+    player1_turn()
+
 main()
 
