f4fb75fc35335367b412e7605a1d73181f61c2c1
Diana Dovhun <dianadovgun4@gmail.com>
2025-02-05 15:48:07 +0200

    Finished player1 turn and some modifications

--- main.py
+++ main.py
@@ -7,15 +7,41 @@ def print_game_rules():
     print("Cast fireball ( Costs 40 mana and deals 30 damage to the other player )")
     print("Restore mana ( Restores 20 mana )")
 
-def player1_turn():
+def print_stats(stats):
+    print("\nPlayers info: \n Player 1 health - {player1_health} \n Player 1 mana - {player1_mana} \n \n Player 2 health - {player2_health} \n Player 2 mana - {player2_mana}".format(**stats))
+
+def player1_turn(stats):
     name1 = input("\n Enter your name,Player 1: ")
     print("Choose your move,", name1,":", end='')
-    player_choice1 = input(" Attack,Heal,Cast fireball or Restore mana? ")
+    player_choice1 = input(" Attack,Heal,Cast fireball or Restore mana?")
 
     if player_choice1 == "Attack":
-        print("You dealt: ",random.choice(range(10, 20)), " damage")
+        damage = random.choice(range(10, 20))
+        stats["player2_health"] -= damage
+        print("You dealt: ", damage, " damage")
+        print_stats(stats)
+    elif player_choice1 == "Heal":
+        stats["player1_health"] += 10
+        print(" Your health now is: ", stats["player1_health"])
+        print_stats(stats)
+    elif player_choice1 == "Cast fireball":
+        stats["player1_mana"] -= 40
+        stats["player2_health"] -= 30
+        print("Damage dealt: 30")
+        print_stats(stats)
+    elif player_choice1 == "Restore mana":
+        stats["player1_mana"] += 20
+        print("You restored 20 mana")
+        print_stats(stats)
 
 def main():
+    stats = {
+        "player1_health" :100,
+        "player2_health" :100,
+        "player1_mana" :100,
+        "player2_mana" :100
+    }
+
     print_game_rules()
 
     answer = input("\n Let's start?: ")
@@ -23,12 +49,13 @@ def main():
     answer = answer.lower()
 
     if answer == "yes":
-        print("\nPlayers info: \n Player 1 health - 100 \n Player 1 mana - 100 \n \n Player 2 health - 100 \n Player 2 mana - 100")
+        print_stats(stats)
     else:
         print("See you later!")
         exit()
 
-    player1_turn()
+    player1_turn(stats)
+
 
 main()
 
